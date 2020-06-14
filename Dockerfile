14880c4176e108bc0000c17e20e57b198ea25ea5
Santaionut <adysanta@gmail.com>
2020-06-14 19:38:39 +0300

    update zuul implementation

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:8-jdk-alpine
 EXPOSE 8080
 ARG JAR_FILE=target/zuul-server-*.jar
-ADD ${JAR_FILE} zuul-server.jar
-ENTRYPOINT ["java","-jar","/zuul-server.jar"]
+ADD ${JAR_FILE} zuul-service.jar
+ENTRYPOINT ["java","-jar","/zuul-service.jar"]
--- src/main/java/com/asanta/zuul/ZuulServerApplication.java
+++ src/main/java/com/asanta/zuul/ZuulServerApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.core.io.ClassPathResource;
 
-import java.net.MalformedURLException;
 
 
 @EnableZuulProxy
@@ -16,12 +14,8 @@ import java.net.MalformedURLException;
 @EnableEurekaClient
 public class ZuulServerApplication {
 	
-	public static void main(String[] args) throws MalformedURLException {
-
-//        Object[] sources = {SpringCloudZuulExampleApplication.class, new UrlResource(new URL("file:C:/dev/abc_dynamic.groovy"))};
-		Object[] sources = {ZuulServerApplication.class, new ClassPathResource ("groovy/ExampleSurgicalDebugFilterBean.groovy")};
-		SpringApplication.run(( Class < ? >[] ) sources , args);
-		
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServerApplication.class, args);
 	}
 	
 	
